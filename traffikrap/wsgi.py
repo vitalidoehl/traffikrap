2c90331437ed0429161f2b8bd443e05249170c97
Patrik <pmueller@hatraco.de>
2018-04-13 16:06:00 +0200

    clean wsgi

--- traffikrap/wsgi.py
+++ traffikrap/wsgi.py
@@ -1,23 +1,6 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
-import sys
-import site
-import django.core.handlers.wsgi
 
-# Add the site-packages of the chosen virtualenv to work with
-site.addsitedir('/home/traffik-rap/.virtualenvs/traffikrap/lib/python2.7/site-packages')
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-# Add the app's directory to the PYTHONPATH
-sys.path.append('/home/traffik-rap/www/traffikrap/traffikrap')
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
-
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
